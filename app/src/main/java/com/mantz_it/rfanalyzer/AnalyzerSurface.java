43fe944562831ce8d47476840c196f849420aa22
Sorin Popescu <sorin-cristian.popescu@keysight.com>
2024-08-19 20:04:47 +0300

    Reduce default FFT dB scale range

--- app/src/main/java/com/mantz_it/rfanalyzer/AnalyzerSurface.java
+++ app/src/main/java/com/mantz_it/rfanalyzer/AnalyzerSurface.java
@@ -103,8 +103,8 @@ public class AnalyzerSurface extends SurfaceView implements SurfaceHolder.Callba
 	// the actual values when the user does scrolling and zooming
 	private long virtualFrequency = -1;		// Center frequency of the fft (baseband) AS SHOWN ON SCREEN
 	private int virtualSampleRate = -1;		// Sample Rate of the fft AS SHOWN ON SCREEN
-	private float minDB = -120;				// Lowest dB on the scale
-	private float maxDB = -30;				// Highest dB on the scale
+	private float minDB = -115;				// Lowest dB on the scale
+	private float maxDB = -50;				// Highest dB on the scale
 	private long lastFrequency;				// Center frequency of the last packet of fft samples
 	private int lastSampleRate;				// Sample rate of the last packet of fft samples
 
